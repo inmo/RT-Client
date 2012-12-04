7854f105e3195237d759d5d63c861bf20d3d486e
James Savage <axiixc@gmail.com>
2012-12-03 19:31:48 -0500

    Xcode Template

---
+++ RT Client/RTCAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  RTCAppDelegate.h
+//  RT Client
+//
+//  Created by James Savage on 12/3/12.
+//  Copyright (c) 2012 INMO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RTCAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ RT Client/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RT Client
+//
+//  Created by James Savage on 12/3/12.
+//  Copyright (c) 2012 INMO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
