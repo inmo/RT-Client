48b39df013584a868407473c4cd0bbccb76ae73e
James Savage <axiixc@gmail.com>
2013-03-22 13:11:05 -0400

    Updated project with new test target config

---
+++ RT Tests/RT_Tests.h
@@ -0,0 +1,13 @@
+//
+//  RT_Tests.h
+//  RT Tests
+//
+//  Created by James Savage on 3/22/13.
+//  Copyright (c) 2013 INMO. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RT_Tests : SenTestCase
+
+@end
---
+++ RT Tests/RT_Tests.m
@@ -0,0 +1,32 @@
+//
+//  RT_Tests.m
+//  RT Tests
+//
+//  Created by James Savage on 3/22/13.
+//  Copyright (c) 2013 INMO. All rights reserved.
+//
+
+#import "RT_Tests.h"
+
+@implementation RT_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RT Tests");
+}
+
+@end
